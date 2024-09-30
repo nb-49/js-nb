d32aef485d67e708477d21d05c02500b26e66a5d
nb-49 <neerajbhandari2700@gmail.com>
2024-09-30 12:09:24 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14435
+let accountEmail = "neeraj@gmail.com"
+var accoutPassword = "2234"
+accountCity = "Jaipur"
+let accountState
+
+
+//accountId = 2234
+
+console.table([accountId, accountEmail,accoutPassword,accountCity, accountState])
